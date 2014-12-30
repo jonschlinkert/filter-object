53e72bdc405a95a2cf609c32f8758bae2190c9d3
jonschlinkert <jon.schlinkert@sellside.com>
2014-12-30 05:35:34 -0500

    bump deps

--- test.js
+++ test.js
@@ -1,38 +1,32 @@
 /*!
  * filter-object <https://github.com/jonschlinkert/filter-object>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT License
  */
 
 'use strict';
 
-var should = require('should');
+var assert = require('assert');
 var filter = require('./');
 
 describe('filter', function () {
   it('should filter keys using the given glob patterns', function () {
-    filter({a: 'a', b: 'b', c: 'c'}, '*').should.eql({a: 'a', b: 'b', c: 'c'});
-    filter({a: 'a', b: 'b', c: 'c'}, 'b').should.eql({b: 'b'});
-    filter({foo: 'a', bar: 'b', baz: 'c'}, 'b*').should.eql({bar: 'b', baz: 'c'});
+    assert.deepEqual(filter({a: 'a', b: 'b', c: 'c'}, '*'), {a: 'a', b: 'b', c: 'c'});
+    assert.deepEqual(filter({a: 'a', b: 'b', c: 'c'}, 'b'), {b: 'b'});
+    assert.deepEqual(filter({foo: 'a', bar: 'b', baz: 'c'}, 'b*'), {bar: 'b', baz: 'c'});
   });
 
   it('should exclude keys that match negation patterns:', function () {
-    filter({a: 'a', b: 'b', c: 'c'}, ['*', '!a']).should.eql({b: 'b', c: 'c'});
-    filter({foo: 'a', bar: 'b', baz: 'c'}, ['*', '!b*']).should.eql({foo: 'a'});
+    assert.deepEqual(filter({a: 'a', b: 'b', c: 'c'}, ['*', '!a']), {b: 'b', c: 'c'});
+    assert.deepEqual(filter({foo: 'a', bar: 'b', baz: 'c'}, ['*', '!b*']), {foo: 'a'});
   });
 
   it('should filter keys using brace expansion', function () {
-    filter({a: 'a', b: 'b', c: 'c'}, '{b,c}').should.eql({b: 'b', c: 'c'});
+    assert.deepEqual(filter({a: 'a', b: 'b', c: 'c'}, '{b,c}'), {b: 'b', c: 'c'});
   });
 
   it('should return the entire object if no pattern is given:', function () {
-    filter({a: 'a', b: 'b', c: 'c'}).should.eql({a: 'a', b: 'b', c: 'c'});
-  });
-
-  it('should throw an error if an object is not passed:', function () {
-    (function () {
-      filter()
-    }).should.throw('filter-object expects an object');
+    assert.deepEqual(filter({a: 'a', b: 'b', c: 'c'}), {a: 'a', b: 'b', c: 'c'});
   });
 });
